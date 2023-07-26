caf5d71dd1852fb709933f2ff2e65e9ababeeb4c
falconcodes <prathamesh.chavanpsc2018@gmail.com>
2023-07-26 15:08:37 +0530

    init: Project setup,Theme,Tailwind prettier config

---
+++ prettier.config.cjs
@@ -0,0 +1,11 @@
+const config = {
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    tailwindConfig: './tailwind.config.js',
+    plugins: ['prettier-plugin-tailwindcss'],
+    pluginSearchDirs: false,
+}
+
+module.exports = config

59f0cb5a12c6f6ba01721ddbef557d7ce2319319
falconcodes <prathamesh.chavanpsc2018@gmail.com>
2023-08-05 20:42:35 +0530

    refactor: page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
     return (
         <>
-            <header className="mb-4 flex items-center justify-between bg-red-200 ">
+            <header className="flex items-center justify-between mb-4 bg-red-200 ">
                 <h1 className="text-2xl">Todos</h1>
             </header>
         </>
